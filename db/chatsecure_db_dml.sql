016b0110883c444bf280b2f9db4591d46fc69d49
Juan Chaparro <juanchagu66@gmail.com>
2023-12-25 14:03:52 -0500

    Adding DML and Updating GITIGNORE

---
+++ db/chatsecure_db_dml.sql
@@ -0,0 +1,37 @@
+USE chatsecure_db;
+
+-- C0nTras3n4S3guRa
+
+INSERT INTO
+  `usuarios` (
+    `id_usuario`,
+    `nombres`,
+    `apellidos`,
+    `correo`,
+    `contrasena`
+  )
+VALUES
+  (
+    NULL,
+    'John',
+    'Doe',
+    'johndoe@cryptoproject.com',
+    '$2b$12$2j/hTcndFzQf4EMrDCJlP.nzigI0eMOTGcUm7oCMA.TH3hCte1YFe'
+  )
+
+INSERT INTO
+  `usuarios` (
+    `id_usuario`,
+    `nombres`,
+    `apellidos`,
+    `correo`,
+    `contrasena`
+  )
+VALUES
+  (
+    NULL,
+    'Jane',
+    'Doe',
+    'janedoe@cryptoproject.com',
+    '$2b$12$2j/hTcndFzQf4EMrDCJlP.nzigI0eMOTGcUm7oCMA.TH3hCte1YFe'
+  )
